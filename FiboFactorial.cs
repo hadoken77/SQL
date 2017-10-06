90fe32865efad3913eacef417f88cab862d031b4
hadoken77 <32001472+hadoken77@users.noreply.github.com>
2017-10-05 22:12:49 -0700

    Update FiboFactorial.cs

--- FiboFactorial.cs
+++ FiboFactorial.cs
@@ -1,5 +1,9 @@
-﻿//Rextester.Program.Main is the entry point for your code. Don't change it.
-//Compiler version 4.0.30319.17929 for Microsoft (R) .NET Framework 4.5
+/*
+Man Nguyen 
+
+These are some questions I use for interviews
+
+*/
 
 using System;
 using System.Collections.Generic;
